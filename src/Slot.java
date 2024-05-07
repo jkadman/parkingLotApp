16a256654bb9e3557623b8f64a7ff055bff975c3
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 16:51:04 -0700

    add Slot fields

--- src/Slot.java
+++ src/Slot.java
@@ -1,2 +1,11 @@
 public class Slot {
+    String type;
+    String vehicle;
+    String ticketId;
+
+    public Slot(String type, String vehicle, String ticketId) {
+        this.type = type;
+        this.vehicle = vehicle;
+        this.ticketId = ticketId;
+    }
 }
