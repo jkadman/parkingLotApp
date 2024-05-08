d12e2c5ee700fc69e6eea1cb06662927c281e992
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 20:56:37 -0700

    call all the methods in ParkingLot

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,18 @@ public class Main {
 
         String ticket1 = first.parkVehicle("car", "78", "blue");
         String ticket2 = first.parkVehicle("truck", "1009", "red");
+        String ticket3 = first.parkVehicle("car", "22", "green");
+
+        int noOfSpots1 = first.getNoOfOpenSlots("car");
+        int noOfSpotstruck1 = first.getNoOfOpenSlots("truck");
+
 
         first.displayOccupiedSlots("car");
         first.displayOccupiedSlots("truck");
+
+        System.out.println("The amount of spaces available for a car is " + noOfSpots1);
+        System.out.println("The amount of spaces available for a truck is " + noOfSpotstruck1);
+
+        first.displayOpenSlots("car");
     }
 }
