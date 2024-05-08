3975742aef54a8ccac2927c21a0312108848f941
jacinth54 <159411510+jacinth54@users.noreply.github.com>
2024-05-07 18:53:54 -0700

    remove uneeded code from Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         ParkingLot first = new ParkingLot("77", 5, 5);
-//        System.out.println("Hello world!");
-        System.out.println(first.getId());
-        System.out.println(first.nfloors);
+
     }
 }
